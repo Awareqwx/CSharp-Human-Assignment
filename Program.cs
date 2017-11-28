6fc116530343fc20d00a66de9bf18734e7a0c11c
Awareqwx <benpeters97@yahoo.com>
2017-11-28 11:43:03 -0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h = new Human();
+        }
+    }
+    public class Human
+    {
+
+        private int strength, intelligence, stealth, health;
+        private string name;
+        
+        public Human()
+        {
+            initialize();
+        }
+
+        public Human(string name)
+        {
+            initialize();
+            this.name = name;
+        }
+
+        public Human(int strength, int intelligence, int stealth, int health, string name)
+        {
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.stealth = stealth;
+            this.health = health;
+            this.name = name;
+        }
+
+        private void initialize()
+        {
+            strength = intelligence = stealth = 3;
+            health = 100;
+        }
+        
+        public int getHealth()
+        {
+            return health;
+        }
+        
+        public void attack(Object h)
+        {
+            if(h is Human)
+            {
+                (h as Human).health -= 5 * strength;
+            }
+        }
+
+    }   
+}
